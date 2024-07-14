4b414d87f98c8b4292872d7b87dfc983820f2adc
Amayuru1999 <amarasingheau@gmail.com>
2024-07-14 13:52:33 +0530

    Created create-user.dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,28 @@
-export class CreateUserDto {}
+import { IsIn, IsNotEmpty, IsOptional, IsString } from "class-validator";
+import { userTypes } from "src/shared/schema/users";
+
+export class CreateUserDto {
+
+    @IsNotEmpty()
+    @IsString()
+    name: string;
+
+    @IsNotEmpty()
+    @IsString()
+    email: string;
+
+    @IsNotEmpty()
+    @IsString()
+    password: string;
+
+    @IsNotEmpty()
+    @IsString()
+    @IsIn([userTypes.ADMIN,userTypes.CUSTOMER])
+    type: string;
+
+    @IsString()
+    @IsOptional()
+    secretToken?: string;
+
+    isVerified?: boolean;
+}
