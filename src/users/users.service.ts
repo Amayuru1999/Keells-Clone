c996347581c73aa8f81b9c6385af7f54b9ab2d83
Amayuru1999 <amarasingheau@gmail.com>
2024-07-14 17:34:15 +0530

    Created Utility Folder

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,89 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  HttpCode,
+  HttpStatus,
+  Res,
+  Put,
+  Query,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { Response } from 'express';
+import { Roles } from 'src/shared/middleware/role.decorators';
+import { userTypes } from 'src/shared/schema/users';
 
-@Injectable()
-export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  async create(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.create(createUserDto);
   }
 
-  findAll() {
-    return `This action returns all users`;
+  @Post('/login')
+  @HttpCode(HttpStatus.OK)
+  async login(
+    @Body() loginUser: { email: string; password: string },
+    @Res({ passthrough: true }) response: Response,
+  ) {
+    const loginRes = await this.usersService.login(
+      loginUser.email,
+      loginUser.password,
+    );
+    if (loginRes.success) {
+      response.cookie('_digi_auth_token', loginRes.result?.token, {
+        httpOnly: true,
+      });
+    }
+    delete loginRes.result?.token;
+    return loginRes;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  @Get('/verify-email/:otp/:email')
+  async verifyEmail(@Param('otp') otp: string, @Param('email') email: string) {
+    return await this.usersService.verifyEmail(otp, email);
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  @Get('send-otp-email/:email')
+  async sendOtpEmail(@Param('email') email: string) {
+    return await this.usersService.sendOtpEmail(email);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  @Put('/logout')
+  async logout(@Res() res: Response) {
+    res.clearCookie('_digi_auth_token');
+    return res.status(HttpStatus.OK).json({
+      success: true,
+      message: 'Logout successfully',
+    });
+  }
+
+  @Get('forgot-password/:email')
+  async forgotPassword(@Param('email') email: string) {
+    return await this.usersService.forgotPassword(email);
+  }
+
+  @Get()
+  @Roles(userTypes.ADMIN)
+  async findAll(@Query('type') type: string) {
+    return await this.usersService.findAll(type);
+  }
+
+  @Patch('/update-name-password/:id')
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.usersService.updatePasswordOrName(id, updateUserDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.usersService.remove(+id);
   }
 }
