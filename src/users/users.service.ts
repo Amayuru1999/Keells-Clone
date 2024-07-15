fd59a4fa5d450d8337b1f8a9e840aa1cc8ecb09c
Amayuru1999 <amarasingheau@gmail.com>
2024-07-15 20:04:21 +0530

    Users Services Issue Fixed

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,89 +1,314 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-  HttpCode,
-  HttpStatus,
-  Res,
-  Put,
-  Query,
-} from '@nestjs/common';
-import { UsersService } from './users.service';
+import { Inject, Injectable } from '@nestjs/common';
+import { userTypes } from 'src/shared/schema/users';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { Response } from 'express';
-import { Roles } from 'src/shared/middleware/role.decorators';
-import { userTypes } from 'src/shared/schema/users';
+import config from 'config';
+import { UserRepository } from 'src/shared/repositories/user.repository';
+import {
+  comparePassword,
+  generateHashPassword,
+} from 'src/shared/utility/password-manager';
+import { sendEmail } from 'src/shared/utility/mail-handler';
+import { generateAuthToken } from 'src/shared/utility/token-generator';
+@Injectable()
+export class UsersService {
+  constructor(
+    @Inject(UserRepository) private readonly userDB: UserRepository,
+  ) {}
+  async create(createUserDto: CreateUserDto) {
+    try {
+      // generate the hash password
+      createUserDto.password = await generateHashPassword(
+        createUserDto.password,
+      );
 
-@Controller('users')
-export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+      /// check is it for admin
+      if (
+        createUserDto.type === userTypes.ADMIN &&
+        createUserDto.secretToken !== config.get('adminSecretToken')
+      ) {
+        throw new Error('Not allowed to create admin');
+      } else if (createUserDto.type !== userTypes.CUSTOMER) {
+        createUserDto.isVerified = true;
+      }
 
-  @Post()
-  async create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
-  }
-
-  @Post('/login')
-  @HttpCode(HttpStatus.OK)
-  async login(
-    @Body() loginUser: { email: string; password: string },
-    @Res({ passthrough: true }) response: Response,
-  ) {
-    const loginRes = await this.usersService.login(
-      loginUser.email,
-      loginUser.password,
-    );
-    if (loginRes.success) {
-      response.cookie('_digi_auth_token', loginRes.result?.token, {
-        httpOnly: true,
+      // user is already exist
+      const user = await this.userDB.findOne({
+        email: createUserDto.email,
       });
+      if (user) {
+        throw new Error('User already exist');
+      }
+
+      // generate the otp
+      const otp = Math.floor(Math.random() * 900000) + 100000;
+
+      const otpExpiryTime = new Date();
+      otpExpiryTime.setMinutes(otpExpiryTime.getMinutes() + 10);
+
+      const newUser = await this.userDB.create({
+        ...createUserDto,
+        otp,
+        otpExpiryTime,
+      });
+      if (newUser.type !== userTypes.ADMIN) {
+        sendEmail(
+          newUser.email,
+          config.get('emailService.emailTemplates.verifyEmail'),
+          'Email verification - Digizone',
+          {
+            customerName: newUser.name,
+            customerEmail: newUser.email,
+            otp,
+          },
+        );
+      }
+      return {
+        success: true,
+        message:
+          newUser.type === userTypes.ADMIN
+            ? 'Admin created successfully'
+            : 'Please activate your account by verifying your email. We have sent you a wmail with the otp',
+        result: { email: newUser.email },
+      };
+    } catch (error) {
+      throw error;
     }
-    delete loginRes.result?.token;
-    return loginRes;
   }
 
-  @Get('/verify-email/:otp/:email')
-  async verifyEmail(@Param('otp') otp: string, @Param('email') email: string) {
-    return await this.usersService.verifyEmail(otp, email);
+  async login(email: string, password: string) {
+    try {
+      const userExists = await this.userDB.findOne({
+        email,
+      });
+      if (!userExists) {
+        throw new Error('Invalid email or password');
+      }
+      if (!userExists.isVerified) {
+        throw new Error('Please verify your email');
+      }
+      const isPasswordMatch = await comparePassword(
+        password,
+        userExists.password,
+      );
+      if (!isPasswordMatch) {
+        throw new Error('Invalid email or password');
+      }
+      const token = await generateAuthToken(userExists._id);
+
+      return {
+        success: true,
+        message: 'Login successful',
+        result: {
+          user: {
+            name: userExists.name,
+            email: userExists.email,
+            type: userExists.type,
+            id: userExists._id.toString(),
+          },
+          token,
+        },
+      };
+    } catch (error) {
+      throw error;
+    }
   }
 
-  @Get('send-otp-email/:email')
-  async sendOtpEmail(@Param('email') email: string) {
-    return await this.usersService.sendOtpEmail(email);
+  async verifyEmail(otp: string, email: string) {
+    try {
+      const user = await this.userDB.findOne({
+        email,
+      });
+      if (!user) {
+        throw new Error('User not found');
+      }
+      if (user.otp !== otp) {
+        throw new Error('Invalid otp');
+      }
+      if (user.otpExpiryTime < new Date()) {
+        throw new Error('Otp expired');
+      }
+      await this.userDB.updateOne(
+        {
+          email,
+        },
+        {
+          isVerified: true,
+        },
+      );
+
+      return {
+        success: true,
+        message: 'Email verified successfully. you can login now',
+      };
+    } catch (error) {
+      throw error;
+    }
   }
 
-  @Put('/logout')
-  async logout(@Res() res: Response) {
-    res.clearCookie('_digi_auth_token');
-    return res.status(HttpStatus.OK).json({
-      success: true,
-      message: 'Logout successfully',
-    });
+  async sendOtpEmail(email: string) {
+    try {
+      const user = await this.userDB.findOne({
+        email,
+      });
+      if (!user) {
+        throw new Error('User not found');
+      }
+      if (user.isVerified) {
+        throw new Error('Email already verified');
+      }
+      const otp = Math.floor(Math.random() * 900000) + 100000;
+
+      const otpExpiryTime = new Date();
+      otpExpiryTime.setMinutes(otpExpiryTime.getMinutes() + 10);
+
+      await this.userDB.updateOne(
+        {
+          email,
+        },
+        {
+          otp,
+          otpExpiryTime,
+        },
+      );
+
+      sendEmail(
+        user.email,
+        config.get('emailService.emailTemplates.verifyEmail'),
+        'Email verification - Digizone',
+        {
+          customerName: user.name,
+          customerEmail: user.email,
+          otp,
+        },
+      );
+
+      return {
+        success: true,
+        message: 'Otp sent successfully',
+        result: { email: user.email },
+      };
+    } catch (error) {
+      throw error;
+    }
   }
 
-  @Get('forgot-password/:email')
-  async forgotPassword(@Param('email') email: string) {
-    return await this.usersService.forgotPassword(email);
+  async forgotPassword(email: string) {
+    try {
+      const user = await this.userDB.findOne({
+        email,
+      });
+      if (!user) {
+        throw new Error('User not found');
+      }
+      let password = Math.random().toString(36).substring(2, 12);
+      const tempPassword = password;
+      password = await generateHashPassword(password);
+      await this.userDB.updateOne(
+        {
+          _id: user._id,
+        },
+        {
+          password,
+        },
+      );
+
+      sendEmail(
+        user.email,
+        config.get('emailService.emailTemplates.forgotPassword'),
+        'Forgot password - Digizone',
+        {
+          customerName: user.name,
+          customerEmail: user.email,
+          newPassword: password,
+          loginLink: config.get('loginLink'),
+        },
+      );
+
+      return {
+        success: true,
+        message: 'Password sent to your email',
+        result: { email: user.email, password: tempPassword },
+      };
+    } catch (error) {
+      throw error;
+    }
   }
 
-  @Get()
-  @Roles(userTypes.ADMIN)
-  async findAll(@Query('type') type: string) {
-    return await this.usersService.findAll(type);
+  async findAll(type: string) {
+    try {
+      const users = await this.userDB.find({
+        type,
+      });
+      return {
+        success: true,
+        message: 'Users fetched successfully',
+        result: users,
+      };
+    } catch (error) {
+      throw error;
+    }
   }
 
-  @Patch('/update-name-password/:id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.updatePasswordOrName(id, updateUserDto);
+  async updatePasswordOrName(
+    id: string,
+    updatePasswordOrNameDto: UpdateUserDto,
+  ) {
+    try {
+      const { oldPassword, newPassword, name } = updatePasswordOrNameDto;
+      if (!name && !newPassword) {
+        throw new Error('Please provide name or password');
+      }
+      const user = await this.userDB.findOne({
+        _id: id,
+      });
+      if (!user) {
+        throw new Error('User not found');
+      }
+      if (newPassword) {
+        const isPasswordMatch = await comparePassword(
+          oldPassword,
+          user.password,
+        );
+        if (!isPasswordMatch) {
+          throw new Error('Invalid current password');
+        }
+        const password = await generateHashPassword(newPassword);
+        await this.userDB.updateOne(
+          {
+            _id: id,
+          },
+          {
+            password,
+          },
+        );
+      }
+      if (name) {
+        await this.userDB.updateOne(
+          {
+            _id: id,
+          },
+          {
+            name,
+          },
+        );
+      }
+      return {
+        success: true,
+        message: 'User updated successfully',
+        result: {
+          name: user.name,
+          email: user.email,
+          type: user.type,
+          id: user._id.toString(),
+        },
+      };
+    } catch (error) {
+      throw error;
+    }
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
+  remove(id: number) {
+    return `This action removes a #${id} user`;
   }
 }
